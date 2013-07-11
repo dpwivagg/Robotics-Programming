5144e9d184418da7bb169ec961d33d2d10b989d2
dpwivagg <dpwivagg@gmail.com>
2013-07-11 16:50:12 -0400

    A header file for basic and routine movement commands

---
+++ movement.h
@@ -0,0 +1,23 @@
+#pragma config(Sensor, in6,    clawAngle,      sensorPotentiometer)
+#pragma config(Sensor, dgtl1,  rightEncoder,   sensorQuadEncoder)
+#pragma config(Sensor, dgtl3,  leftEncoder,    sensorQuadEncoder)
+#pragma config(Sensor, dgtl8,  sonarInput,     sensorSONAR_inch)
+#pragma config(Motor,  port1,           rightMotor,    tmotorVex393, openLoop, reversed)
+#pragma config(Motor,  port3,           claw,          tmotorVex393, openLoop)
+#pragma config(Motor,  port10,          leftMotor,     tmotorVex393, openLoop)
+
+int x;
+
+int y;
+
+int z;
+
+void rightAngleTurnRight();
+
+void rightAngleTurnLeft();
+
+void straightLine(int distance);
+
+void straightLine();
+
+void pause(int seconds);
